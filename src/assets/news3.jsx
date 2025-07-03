f0df51d2aa961b0263a2f1d34a774f3ad2d889ba
ASHISH RANA <ashishrana30012000@gmail.com>
2025-07-03 15:10:33 +0530

    file update

--- src/assets/news3.jsx
+++ src/assets/news3.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { fetchAPI1 } from '../APIs/APIS';
 
-export default function Home() {
+export default function news3() {
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
